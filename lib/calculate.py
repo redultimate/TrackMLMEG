865442435a4d944c6b30efe1444c9c521a61baae
Rina Onda <rinaonda@RinanoMBP.psi.ch>
2018-06-26 09:32:09 +0200

    assembly all

--- lib/calculate.py
+++ lib/calculate.py
@@ -3,23 +3,20 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 
 class CalculatorCells:
-	def sum_edep(cells):
-		n = len(hits.hit_id.values)
+	def total_edep(self,cells):
 		edeps = []
 		for i in range(len(cells.hit_id.unique())):
 			edep = cells.value[cells.hit_id==i]
 			edep_sum = sum(edep)
 			edeps = np.append(edeps,edep_sum)
-            
+
 		return edeps
 
 class CalculatorParticles:
-	def momentum(particles):
+	def momentum(self,particles):
 		px = particles.px.values
 		py = particles.py.values
 		pz = particles.pz.values
 		p = np.sqrt(px*px + py*py + pz*pz)
-
-		particles['p'] = p
 
 		return p	
--- lib/convert.py
+++ lib/convert.py
@@ -1,7 +1,9 @@
 from calculate import CalculatorCells as CalCells
 
 class Converter:
-	def particles2truth(particles, truth):
+	def particles2truth(self, particles, truth):
+		assembly = truth
+
 		particle_id = particles.particle_id.values
 		vx = [0 for i in range(len(truth.hit_id.values))]
 		vy = [0 for i in range(len(truth.hit_id.values))]
@@ -10,10 +12,10 @@ class Converter:
 		py = [0 for i in range(len(truth.hit_id.values))]
 		pz = [0 for i in range(len(truth.hit_id.values))]
 		q = [0 for i in range(len(truth.hit_id.values))]
-		nhit = [0 for i in range(len(truth.hit_id.values))]
+		nhits = [0 for i in range(len(truth.hit_id.values))]
 
 		for i in range(len(particle_id)):
-			hit_id = truth.hit_id[truth.particle_id == particle_id[i]]
+			hit_id = assembly.hit_id[assembly.particle_id == particle_id[i]]
 			if len(hit_id) == 0:
 				continue
         
@@ -24,7 +26,7 @@ class Converter:
 			py2   = particles.py[particles.particle_id == particle_id[i]]
 			pz2   = particles.pz[particles.particle_id == particle_id[i]]
 			q2    = particles.q[particles.particle_id == particle_id[i]]
-			nhit2 = particles.nhit[particles.particle_id == particle_id[i]]
+			nhits2 = particles.nhits[particles.particle_id == particle_id[i]]
         
 			for j in range(len(hit_id)):
 				vx[hit_id.values[j]-1] = vx2.values[0]
@@ -34,35 +36,42 @@ class Converter:
 				py[hit_id.values[j]-1] = py2.values[0]
 				pz[hit_id.values[j]-1] = pz2.values[0]
 				q[hit_id.values[j]-1] = q2.values[0]
-				nhit[hit_id.values[j]-1] = nhit2.values[0]
+				nhits[hit_id.values[j]-1] = nhits2.values[0]
         
-			truth['vx'] = vx
-			truth['vy'] = vy
-			truth['vz'] = vz
-			truth['px'] = px
-			truth['py'] = py
-			truth['pz'] = pz
-			truth['q'] = q
-			truth['nhit'] = nhit
+			assembly['vx'] = vx
+			assembly['vy'] = vy
+			assembly['vz'] = vz
+			assembly['px'] = px
+			assembly['py'] = py
+			assembly['pz'] = pz
+			assembly['q'] = q
+			assembly['nhits'] = nhits
         
-		return 
+		return assembly
 
-	def truth2hits(truth,hits):
-		hits['particle_id'] = truth.particle_id.values
-		hits['tx'] = truth.tx.values
-		hits['ty'] = truth.ty.values
-		hits['tz'] = truth.tz.values
-		hits['tpx'] = truth.tpx.values
-		hits['tpy'] = truth.tpy.values
-		hits['tpz'] = truth.tpz.values
-		hits['weight'] = truth.weight.values
+	def truth2hits(self,truth,hits):
+		assembly = hits
+		keys = truth.keys()
+		for i in range(len(keys)):
+			assembly[keys[i]] = truth[keys[i]].values
     
-		return
+		return assembly
 
-	def cells2hits(cells,hits):
+	def cells2hits(self,cells,hits):
+		assembly = hits
 		total_edep = CalCells.sum_edep(cells)
-		hits['total_edep'] = total_edep
+		assembly['total_edep'] = total_edep
 
-		return
+		return assembly
 
-		
+	def AssembleAll(self,hits,cells,particles,truth):
+		assembly = self.cells2hits(cells,hits)
+		assembly2 = self.particles2truth(particles,truth)
+
+		keys = assembly2.keys();
+		for i in range(len(keys)):
+			assembly[keys[i]] = assembly2[keys[i]].values
+
+		return assembly
+	
+	
