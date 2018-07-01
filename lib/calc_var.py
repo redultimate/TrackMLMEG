6bdcdf7e458a19ef37d0aedc69d3c4825d0cb358
Shinji Ogawa <ogawa@icepp.s.u-tokyo.ac.jp>
2018-07-01 15:26:35 +0000

    Add a lib to calc basic variables.

---
+++ lib/calc_var.py
@@ -0,0 +1,50 @@
+"""
+Usage:
+hits = (cv.MeasCalculator()).calc_all(hits)
+hits = (cv.MCCalculator()).calc_all(hits)
+"""
+
+import numpy as np
+
+class MeasCalculator:
+   #_____________________________________________________________
+   def __init__(self):
+      return
+   #_____________________________________________________________
+   def calc_all(self, hits):
+      hits = self.calc_cylindrical(hits)
+      hits = self.calc_xyz2(hits)
+      return hits
+   #_____________________________________________________________
+   def calc_cylindrical(self, hits):
+      hits['hit_r'] = np.sqrt(hits['x'] **2+ hits['y']**2)
+      hits['hit_theta'] = np.degrees(np.arctan2(hits['x'], hits['y']))
+      hits['hit_phi'] = np.degrees(np.arctan2(hits['hit_r'], hits['z']))
+      return hits
+   #_____________________________________________________________
+   def calc_xyz2(self, hits):
+      hits['hit_x2'] = hits['x']/ np.sqrt(hits['x']**2 + hits['y']**2 + hits['z']**2)
+      hits['hit_y2'] = hits['y']/ np.sqrt(hits['x']**2 + hits['y']**2 + hits['z']**2)
+      hits['hit_z2'] = hits['z']/ np.sqrt(hits['x']**2 + hits['y']**2)
+      return hits
+
+class MCCalculator:
+   #_____________________________________________________________
+   def __init__(self):
+      return
+   #_____________________________________________________________
+   def calc_all(self, hits):
+      hits = self.calc_cylindrical(hits)
+      hits = self.calc_trackweight(hits)
+      return hits
+   #_____________________________________________________________
+   def calc_cylindrical(self, hits):
+      hits['hit_vr'] = np.sqrt(hits['vx'] **2+ hits['vy']**2)
+      hits['hit_vtheta'] = np.degrees(np.arctan2(hits['vx'], hits['vy']))
+      hits['hit_vphi'] = np.degrees(np.arctan2(hits['hit_vr'], hits['vz']))
+      return hits
+   #_____________________________________________________________
+   def calc_trackweight(self, hits):
+      trackweights = hits.groupby('particle_id').sum()['weight']
+      hits['track_weight'] = trackweights[hits['particle_id']].reset_index()['weight']
+      return hits
