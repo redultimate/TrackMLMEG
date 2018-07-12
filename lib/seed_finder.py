ccd084a7c197f340d45ea41ad34d248fac314931
Mitsutaka Nakao <nakao@icepp.s.u-tokyo.ac.jp>
2018-07-12 10:33:32 +0200

    completed 1st version of seed finder

--- lib/seed_finder.py
+++ lib/seed_finder.py
@@ -1,44 +1,58 @@
 import numpy as np
 import pandas as pd
+import pickle as pkl
 
+import sys
+sys.path.append("../lib/")
+
+# meg library
+from tracking_utility import *
+
+# see how_to_get_seed_candidates
 # if debug = 1, only 2 hit combinations in each layer will be processed
 
-mask_17_4 = ((hits['volume_id'].values == 17) & (hits['layer_id'].values == 4))
-mask_17_2 = ((hits['volume_id'].values == 17) & (hits['layer_id'].values == 2))
-mask_13_8 = ((hits['volume_id'].values == 13) & (hits['layer_id'].values == 8))
-hits_17_4 = hits[mask_17_4]
-hits_17_2 = hits[mask_17_2]
-hits_13_8 = hits[mask_13_8]
 
-class seed_finder(object):
+class SeedFinder(object):
 
     def __init__(self, thre, debug):
         
         self.thre = thre
         self.debug = debug
 
-    def get_candidates(self, hits):
+    def get_candidates(self, hits, path_to_tables):
+        
+        mask_17_4 = ((hits['volume_id'].values == 17) & (hits['layer_id'].values == 4))
+        mask_17_2 = ((hits['volume_id'].values == 17) & (hits['layer_id'].values == 2))
+        mask_13_8 = ((hits['volume_id'].values == 13) & (hits['layer_id'].values == 8))
+        hits_17_4 = hits[mask_17_4]
+        hits_17_2 = hits[mask_17_2]
+        hits_13_8 = hits[mask_13_8]
+        
+        # read lookup tables
+        f = open("%s/mid_lookup_17_4_to_17_2.pkl" % path_to_tables,"rb")
+        mid_lookup_17_4_to_17_2 = pkl.load(f)
+        f = open("%s/mid_lookup_17_2_to_13_8.pkl" % path_to_tables,"rb")
+        mid_lookup_17_2_to_13_8 = pkl.load(f)
 
         if self.debug:
             nhits1st = 2
         else:
             nhits1st = len(hits_17_4)
 
-        #index in the 1st layer, all the hits in the 1st layer
+        # index in the 1st layer, all the hits in the 1st layer
         for id1st in range(nhits1st):
-
-            #module id in the 1st outer layer (volume17, layer4 in this case)
+            print(id1st)
+            # module id in the 1st outer layer (volume17, layer4 in this case)
             mid1st = hits_17_4['module_id'].values[id1st]
+            true_particle_id = hits_17_4['particle_id'].values[id1st]
             if self.debug:
                 print(id1st, "th hit in the 1st layer")
                 print("module ", mid1st, "in the 1st layer")
                 print("(1st layer) [x, y] = ", hits_17_4.loc[:, ['x', 'y']].values[id1st])
                 print("(1st layer) hitid = ", hits_17_4.loc[:, ['hit_id']].values[id1st])
-
-            true_particle_id = hits_17_4['particle_id'].values[id1st]
-            print("true particle_id: ", true_particle_id)
-            #get module id in the 2nd layer which corresponds to the module in the 1st layer according to the look-up table
-            #mid1st-1 because module id is start from 1 not 0
+                print("true particle_id: ", true_particle_id)
+            # get module id in the 2nd layer which corresponds to the module in the 1st layer according to the look-up table
+            # mid1st-1 because module id is start from 1 not 0
             mask2nd= hits_17_2['module_id'].isin(mid_lookup_17_4_to_17_2[mid1st-1])
             hits_17_2_selected = hits_17_2[mask2nd]
 
@@ -46,7 +60,7 @@ class seed_finder(object):
                 nhits2nd= 2
             else:
                 nhits2nd = len(hits_17_2_selected)
-            #loop for 2nd layer (volume17, layer2 in this case)
+            # loop for 2nd layer (volume17, layer2 in this case)
             for id2nd in range(nhits2nd):
                 mid2nd = hits_17_2_selected['module_id'].values[id2nd]
                 if self.debug:
@@ -56,15 +70,15 @@ class seed_finder(object):
                     print(hits_17_2_selected.loc[:, ['x', 'y']].values[id2nd])
                     print(hits_17_2_selected.loc[:, ['hit_id']].values[id2nd])
 
-                #get module id in the 3rd layer which corresponds to the module in the 2nd layer according to the look-up table
+                # get module id in the 3rd layer which corresponds to the module in the 2nd layer according to the look-up table
                 # mid2nd-1 because module id is start from 1 not 0
                 mask3rd = hits_13_8['module_id'].isin(mid_lookup_17_2_to_13_8[mid2nd-1])
                 hits_13_8_selected = hits_13_8[mask3rd]
 
-                #get (x,y) of the selected hits in 3rd layer
+                # get (x,y) of the selected hits in 3rd layer
                 aXYmodule3= np.array(hits_13_8_selected.loc[:, ['x', 'y']])
                 aHitIDmodule3 = np.array(hits_13_8_selected.loc[:, ['hit_id']])
-                #reshape to concat the array to 1st and 2nd layer
+                # reshape to concat the array to 1st and 2nd layer
                 aXYmodule3 = aXYmodule3.reshape(aXYmodule3.shape[0],1,2)
                 aHitIDmodule3 = aHitIDmodule3.reshape(aHitIDmodule3.shape[0],1,1)
 
@@ -72,7 +86,7 @@ class seed_finder(object):
                     print(aXYmodule3.shape)
                     print(aHitIDmodule3.shape)
 
-                #get (x,y) of the specific hit in the 1st and 2nd layer
+                # get (x,y) of the specific hit in the 1st and 2nd layer
                 aXYmodule1_2 = np.array([hits_17_4.loc[:, ['x', 'y']].values[id1st],hits_17_2_selected.loc[:, ['x', 'y']].values[id2nd]])
                 aHitIDmodule1_2 = np.array([hits_17_4.loc[:, ['hit_id']].values[id1st],hits_17_2_selected.loc[:, ['hit_id']].values[id2nd]])
 
@@ -82,7 +96,7 @@ class seed_finder(object):
                     print(aHitIDmodule1_2.shape)
                     print(aHitIDmodule1_2)
 
-                #tile to concat the array to 3rd layer
+                # tile to concat the array to 3rd layer
                 aXYmodule1_2 = np.tile(aXYmodule1_2, [aXYmodule3.shape[0],1,1])
                 aHitIDmodule1_2 = np.tile(aHitIDmodule1_2, [aHitIDmodule3.shape[0],1,1])
 
@@ -90,7 +104,7 @@ class seed_finder(object):
                     print(aXYmodule1_2.shape)
                     print(aHitIDmodule1_2.shape)
 
-                #concat the arrays to calculate track parameters at the same time
+                # concat the arrays to calculate track parameters at the same time
                 aXYmodule2_3 = np.r_['1', aXYmodule1_2, aXYmodule3]
                 aHitIDmodule2_3 = np.r_['1', aHitIDmodule1_2, aHitIDmodule3]
 
@@ -105,8 +119,9 @@ class seed_finder(object):
                     XYmodule2_3 =  np.concatenate((XYmodule2_3, aXYmodule2_3), axis=0)
                     HitIDmodule2_3 =  np.concatenate((HitIDmodule2_3, aHitIDmodule2_3), axis=0)
 
-            print(XYmodule2_3.shape[0], " hit combinations are selected")
-            print(HitIDmodule2_3.shape[0], " hit combinations are selected")
+            if self.debug:
+                print(XYmodule2_3.shape[0], " hit combinations are selected")
+                print(HitIDmodule2_3.shape[0], " hit combinations are selected")
             #true_track = hits[hits['particle_id']==true_particle_id]
             #true_track
             #mask_17_4_true = ((true_track['volume_id'].values == 17) & (true_track['layer_id'].values == 4))
--- lib/seeding_utility.py
+++ lib/seeding_utility.py
@@ -20,13 +20,14 @@ def make_module_tables(vid, path_to_output):
             for j in range(modules_17_2):
                 y = j+1
 
-                if (   (func_data(x, y, 0.79, 239.4) <= width) or
+                if (   ((func_data(x, y, 0.79, 239.4) <= width) and (x < 1500)) or 
                         (func_data(x, y, 0.79, 120.1) <= width) or
                         (func_data(x, y, 0.79, 3.1) <= width) or
-                        (func_data(x, y, 0.79, 363.0) <= width) or
-                        (func_data(x, y, 0.79, -116.4 ) <= width) or
-                        (func_data(x, y, 0.79, - 233.6 ) <= width) or
-                        (func_data(x, y, 0.79, - 354.2 ) <= width)):
+                        ((func_data(x, y, 0.79, 363.0) <= width) and (x < 1500)) or
+                        ((func_data(x, y, 0.79, -116.4 ) <= width) and (x > 1000)) or
+                        ((func_data(x, y, 0.79, - 233.6 ) <= width)  and (x > 2000))or
+                        ((func_data(x, y, 0.79, - 354.2 ) <= width) and (x > 2500))
+                    ):
                     temp.append(y)
             mid_lookup_17_4_to_17_2.append(temp)
         #print(mid_lookup_17_4_to_17_2[211])
@@ -37,12 +38,12 @@ def make_module_tables(vid, path_to_output):
             temp = []
             for j in range(modules_13_8):
                 y = j+1
-
-                if (   (func_data(x, y, 0.85, 0.4) <= width) or
-                        (func_data(x, y, 0.85, 101.5) <= width) or
+                if (   (func_data(x, y, 0.85, 0.4) <= width) or 
+                        ((func_data(x, y, 0.85, 101.5) <= width) and (x < 1500))or
                         (func_data(x, y, 0.85, -100.2) <= width) or
-                        (func_data(x, y, 0.85, 204.1) <= width) or
-                        (func_data(x, y, 0.85, -198.4 ) <= width)):
+                        ((func_data(x, y, 0.85, 204.1) <= width)  and (x < 1000))or
+                        ((func_data(x, y, 0.85, -198.4 ) <= width) and (x > 1500))
+                   ):
                     temp.append(y)
             mid_lookup_17_2_to_13_8.append(temp)
 
