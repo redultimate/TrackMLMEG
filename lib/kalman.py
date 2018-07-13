aad69a13309affa899582753c824ebae292b9361
Rina Onda <rinaonda@RinanoMBP.psi.ch>
2018-07-13 19:47:16 +0200

    update for kalman filter

--- lib/kalman.py
+++ lib/kalman.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import detector as det
 import tracking_utility as tr_ut
+import pandas as pd
 
 class KalmanFilter:
 #    def __init__(self):
@@ -26,8 +27,25 @@ class KalmanFilter:
 
         self.predict_cov += tracks.get_state_cov()
         self.predict_obsrv = tr_ut.get_predict_obsrv(self.predict_state,module_trans)[:,0]
-        #print(obsrv, " - ", self.predict_obsrv)
         chi = obsrv - self.predict_obsrv
+
+        #print('predict: ',self.predict_obsrv, chi)
+        if 1:
+            tmp_predict_state = np.zeros((ndata,ndim)) 
+            invert_state = tr_ut.invert_state(tracks.get_state())
+            for i in range(ndim):
+                #state prediction
+                tmp_predict_state[:,i] = np.sum(state_trans[:,i] * invert_state, axis=1) + state_trans_control[:,i]
+            tmp_predict_obsrv = tr_ut.get_predict_obsrv(tmp_predict_state,module_trans)[:,0]
+            invert = chi**2 > (obsrv - tmp_predict_obsrv)**2
+            self.predict_obsrv[invert] = tmp_predict_obsrv[invert]
+            self.predict_state[invert] = tmp_predict_state[invert]
+
+        tracks.set_state(self.predict_state)
+        chi = obsrv - self.predict_obsrv
+        #print('obsrv: ',obsrv)
+        #print('predict: ',self.predict_obsrv, chi)
+        tracks.set_sum_chi2(tracks.df.sum_chi2.values + chi*chi)
 
         return chi
         
@@ -39,15 +57,19 @@ class KalmanFilter:
         q = np.ones(ndata)
         q[tracks.get_state()[:,0]<0] = -1
         phi = tracks.get_state()[:,2]
-        tracks.get_state()[:,2][(q<0) & (phi<math.pi)] += math.pi
-        tracks.get_state()[:,2][(q<0) & ~(phi<math.pi)] -= math.pi
+        condition = [(q<0) & (phi<math.pi), (q<0) & ~(phi<math.pi)]
+        tracks.get_state()[:,2][condition[0]] += math.pi
+        tracks.get_state()[:,2][condition[1]] -= math.pi
         
         G = np.zeros((ndata,ndim_state,ndim_obsrv))#kalman gain
         #C = np.zeros((ndata,ndim_obsrv,ndim_state))
-        C = tr_ut.get_jacobian(self.predict_state,module_trans)
+        #C = tr_ut.get_jacobian_numerically(tracks.get_state(),module_trans)
+        #print("jacobian ", C)
+        C = tr_ut.get_jacobian(tracks.get_state(),module_trans)
         if C.shape[1] != ndim_obsrv:
-            print("dimension of obsrvation is expected to be ", C.shape[1])
+            print('dimension of obsrvation is expected to be ', C.shape[1])
             return
+        #print("jacobian ", C)
         PC = np.zeros((ndata,ndim_state,ndim_obsrv))
         CPC = np.zeros((ndata,ndim_obsrv,ndim_obsrv))
         GC = np.zeros((ndata,ndim_state,ndim_state))
@@ -55,13 +77,14 @@ class KalmanFilter:
         for i in range(ndim_state):
             for j in range(ndim_obsrv):
                 PC[:,i,j] = np.sum(self.predict_cov[:,i] * C[:,j],axis=1)
+        #print("PC : ",PC)
 
         for i in range(ndim_obsrv):
             for j in range(ndim_obsrv):
                 CPC[:,i,j] = np.sum(C[:,i] * PC[:,:,j],axis=1)
         CPC[:,0,0] += obsrv_cov[:,0] 
         #only for ndim_obsrv=1
-        
+        #print("CPC : ",CPC)
         for i in range(ndim_state):
             for j in range(ndim_obsrv):
                 G[:,i,j] = np.sum(PC[:,i] * np.linalg.inv(CPC)[:,:,j],axis=1)
@@ -79,33 +102,47 @@ class KalmanFilter:
             tracks.get_state_cov()[:,i,i][tracks.get_state_cov()[:,i,i]<1.e-20] = 1.e-20
 
         #copy state vector
-        tracks.set_state(self.predict_state)
-        tracks.get_state()[:,2] = phi
-        tracks.get_state()[:,2][tracks.get_state()[:,2]>math.pi] -= math.pi
+        #tracks.set_state(self.predict_state)
+        #tracks.get_state()[:,2] = phi
+        #tracks.get_state()[:,2][tracks.get_state()[:,2]>math.pi] -= math.pi
         
         chi = obsrv - self.predict_obsrv
-        #print("obsrv : " , obsrv)
-        #print("predict : ", self.predict_obsrv)
-        #print("chi ", chi)
-        #print("before " , tracks.get_state())
+        #print('obsrv : ' , obsrv)
+        #print('predict : ', self.predict_obsrv)
+        #print('chi ', chi)
+        #print('before ' , tracks.get_state())
+        state_tmp = tracks.get_state()
         for i in range(ndim_state):
-            #print(G[:,i,0])
             #print(G[:,i,0]*chi)
-            tracks.get_state()[:,i] += G[:,i,0] * chi
+            state_tmp[:,i] += G[:,i,0] * chi
             #assume only u observation
-        #print("after " , tracks.get_state())
+        tracks.set_state(state_tmp)
+        #print('after ' , tracks.get_state())
 
         q = np.ones(ndata)
         q[tracks.get_state()[:,0]<0] = -1
         tracks.get_state()[:,2][phi>math.pi] += math.pi
         phi = tracks.get_state()[:,2]
-        tracks.get_state()[:,2][(q<0) & (phi<math.pi)] += math.pi
-        tracks.get_state()[:,2][(q<0) & ~(phi<math.pi)] -= math.pi
+        condition = [(q<0) & (phi<math.pi), (q<0) & ~(phi<math.pi)]
+        tracks.get_state()[:,2][condition[0]] += math.pi
+        tracks.get_state()[:,2][condition[1]] -= math.pi
 
         self.predict_obsrv = tr_ut.get_predict_obsrv(self.predict_state,module_trans)[:,0]
         chi = obsrv - self.predict_obsrv
 
-        tracks.set_sum_chi2(tracks.get_sum_chi2() + chi*chi)
+        if 0:
+            tmp_predict_state = np.zeros((ndata,ndim_state)) 
+            tmp_predict_cov = np.zeros((ndata,ndim_state,ndim_state)) 
+            invert_state = tr_ut.invert_state(tracks.get_state())
+            tmp_predict_obsrv = tr_ut.get_predict_obsrv(invert_state,module_trans)[:,0]
+            invert = chi**2 > (obsrv - tmp_predict_obsrv)**2
+            self.predict_obsrv[invert] = tmp_predict_obsrv[invert]
+
+        tracks.set_sum_chi2(tracks.df.sum_chi2.values + chi*chi)
 
         return chi 
 
+    def clear(self):
+        del self.predict_state
+        del self.predict_cov
+        del self.predict_obsrv
--- lib/tracking_utility.py
+++ lib/tracking_utility.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import detector as det
+import pandas as pd
 
 def fit_circle(x,y):
     sumx = sum(x)
@@ -30,7 +31,11 @@ def get_1uDp(x,y):
         return (0,0,0)
     r2 = x*x + y*y
     r2_dif = [r2[1] - r2[2],r2[2] - r2[0], r2[0] - r2[1]]
-    phi = math.atan(sum(y * r2_dif)/sum(x *r2_dif))
+    deno = sum(x * r2_dif)
+    #protect from zero divide
+    if deno < 1.e-20:
+        deno = 1.e-20
+    phi = math.atan(sum(y * r2_dif)/deno)
     if phi < 0:
         phi += math.pi
 
@@ -43,8 +48,17 @@ def get_1uDp(x,y):
     u = 1.0/math.sqrt(rr)
     D = math.sqrt(rr) - rd
 
-    ATan0 = math.atan((y[0]-rd*math.cos(phi))/(x[0]+rd*math.sin(phi))) 
-    ATan1 = math.atan((y[1]-rd*math.cos(phi))/(x[1]+rd*math.sin(phi))) 
+    deno = x[0]+rd*math.sin(phi)
+    #protect from zero divide
+    if deno < 1.e-20:
+        deno = 1.e-20
+    ATan0 = math.atan((y[0]-rd*math.cos(phi))/deno) 
+
+    deno = x[1]+rd*math.sin(phi)
+    #protect from zero divide
+    if deno < 1.e-20:
+        deno = 1.e-20
+    ATan1 = math.atan((y[1]-rd*math.cos(phi))/deno)
     if x[0] < -rd*math.sin(phi):
         ATan0 += math.pi
         if x[1] < -rd*math.sin(phi):
@@ -65,7 +79,7 @@ def get_1uDp(x,y):
             ATan0 += 2*math.sin(phi)
 
     q = 1
-    if ATan1 > ATan0:
+    if ATan1 < ATan0:
         q = -1
     
     #z is not used
@@ -141,7 +155,7 @@ def get_uDp(x):
     #        ATan0 += 2*math.pi
 
     q = np.ones(x.shape[0])
-    q[ATan1>ATan0] = -1
+    q[ATan1<ATan0] = -1
     #if ATan1 > ATan0:
     #    q = -1
     
@@ -166,10 +180,10 @@ def get_jacobian(state,module_trans):
     ndim_state = state.shape[1]
     ndim_obsrv = 1
     
-    cx = module_trans["cx"]
-    cy = module_trans["cy"]
-    cos_theta = module_trans["cos_theta"]
-    sin_theta = module_trans["sin_theta"]
+    cx = module_trans['cx']
+    cy = module_trans['cy']
+    cos_theta = module_trans['cos_theta']
+    sin_theta = module_trans['sin_theta']
     #protect from 0 divide
     state[:,0][state[:,0]==0] = 1e-20
 
@@ -190,14 +204,11 @@ def get_jacobian(state,module_trans):
     cos_phi[phi>math.pi] = np.cos(phi[phi>math.pi]-math.pi)
     sin_phi[phi>math.pi] = np.sin(phi[phi>math.pi]-math.pi)
 
-    #print(q)
     #calculate jacobian
     jacobian = np.zeros((ndata,ndim_obsrv,ndim_state))
     jacobian[:,0,0] = (sin_phi*cos_theta - cos_phi*sin_theta)/state[:,0]/state[:,0]
-    #print(jacobian[:,0,0])
     tmp = (sin_phi*sin_theta - cos_phi*sin_theta)*((cx-ax)*sin_theta - (cy-ay)*cos_theta) - 1./state[:,0]
     tmp /= root*state[:,0]*state[:,0]
-    #print(tmp)
     jacobian[:,0,0][q<0] -= tmp[q<0]
     jacobian[:,0,0][q>0] += tmp[q>0]
     #print(jacobian[:,0,0])
@@ -224,15 +235,28 @@ def get_jacobian(state,module_trans):
 
     return jacobian
 
+def get_jacobian_numerically(state,module_trans):
+    eps = 1.e-10
+
+    jacobian = np.zeros((state.shape[0],1,state.shape[1]))
+    obsrv = get_predict_obsrv(state,module_trans)
+    for i in range(state.shape[1]):
+        state_tmp = state
+        state_tmp[:,i] += state[:,i]*eps
+        jacobian[:,0,i] = get_predict_obsrv(state_tmp,module_trans)[:,0] - obsrv[:,0]
+        jacobian[:,0,i] /= state[:,i]*eps
+
+    return jacobian
+
 def get_predict_obsrv(state,module_trans):
     ndata = state.shape[0]
     ndim_state = state.shape[1]
     ndim_obsrv = 1
     
-    cx = module_trans["cx"]
-    cy = module_trans["cy"]
-    cos_theta = module_trans["cos_theta"]
-    sin_theta = module_trans["sin_theta"]
+    cx = module_trans['cx']
+    cy = module_trans['cy']
+    cos_theta = module_trans['cos_theta']
+    sin_theta = module_trans['sin_theta']
     #protect from 0 divide
     state[:,0][state[:,0]==0] = 1e-20
     #define parameters
@@ -246,7 +270,7 @@ def get_predict_obsrv(state,module_trans):
     sin_phi = np.sin(phi)
     ax = -q * (1./state[:,0] - state[:,1])*sin_phi
     ay = q * (1./state[:,0] - state[:,1])*cos_phi
-    #print("cos ",cos_phi, "sin ",sin_phi, "ax ",ax,"ay ",ay)
+    #print('cos ',cos_phi, 'sin ',sin_phi, 'ax ',ax,'ay ',ay)
     root = 1./state[:,0]/state[:,0] - ((cx-ax)*sin_theta - (cy-ay)*cos_theta)**2
     root[root>0] = np.sqrt(root[root>0])
     root[root<0] = 1.e-20
@@ -254,88 +278,196 @@ def get_predict_obsrv(state,module_trans):
     #sin_phi[phi>math.pi] = np.sin(phi[phi>math.pi]-math.pi)
 
     #calculate hit position in local
-    #print("root ",root)
+    #print('root ',root)
     predict_obsrv = np.zeros((ndata,ndim_obsrv))
     predict_obsrv[:,0] = -(cx-ax)*cos_theta - (cy-ay)*sin_theta
-    #print("cos ",cos_theta, "sin ",sin_theta, "cx ",cx,"cy ",cy)
-    #print("obsrv ", predict_obsrv[:,0])
+    #print('cos ',cos_theta, 'sin ',sin_theta, 'cx ',cx,'cy ',cy)
+    #print('obsrv ', predict_obsrv[:,0])
     #print(q)
     predict_obsrv[:,0][q<0] -= root[q<0]
     predict_obsrv[:,0][q>0] += root[q>0]
 
     return predict_obsrv
 
+def invert_state(state):
+    ndata = state.shape[0]
+    ndim_state = state.shape[1]
+    q = np.ones(ndata)
+    q[state[:,0]<0] = -1
+    phi = state[:,2]
+    condition = [(q<0) & (phi>math.pi), (q<0) & ~(phi>math.pi)]
+    phi[condition[0]] -= math.pi
+    phi[condition[1]] += math.pi
+    condition = [(q>0) & (phi>math.pi), (q>0) & ~(phi>math.pi)]
+    phi[condition[0]] -= math.pi
+    phi[condition[1]] += math.pi
+    state[:,0] *= -1
+    state[:,1] *= -1
+    state[:,2] = phi
+
+    return state
+
+def get_1uDp_byfit(x,y):
+    sumx = sum(x)
+    sumy = sum(y)
+    sumx2 = sum([ix ** 2 for ix in x])
+    sumy2 = sum([iy ** 2 for iy in y])
+    sumxy = sum([ix * iy for (ix,iy) in zip(x,y)])
+
+    F = np.array([[sumx2,sumxy,sumx],
+                  [sumxy,sumy2,sumy],
+                  [sumx, sumy, len(x)]])
+    G = np.array([[-sum([ix ** 3 + ix*iy **2 for (ix,iy) in zip(x,y)])],
+                  [-sum([ix **2 *iy + iy **3 for (ix,iy) in zip(x,y)])],
+                  [-sum([ix ** 2 + iy **2 for (ix,iy) in zip(x,y)])]])
+
+    T = np.linalg.inv(F).dot(G)
+
+    cxe = float(T[0]/-2)
+    cye = float(T[1]/-2)
+    re = math.sqrt(cxe**2 + cye**2 - T[2])
+
+    u = 1./re
+    D = re - np.sqrt(cxe**2 + cye**2)
+    phi = np.arctan(-cxe/cye)
+    if phi < 0:
+        phi += math.pi
+
+    deno = x[0]-cxe
+    #protect from zero divide
+    if deno < 1.e-20:
+        deno = 1.e-20
+    ATan0 = math.atan((y[0]-cye)/deno) 
+
+    deno = x[1]-cxe
+    #protect from zero divide
+    if deno < 1.e-20:
+        deno = 1.e-20
+    ATan1 = math.atan((y[1]-cye)/deno)
+    if x[0] < cxe:
+        ATan0 += math.pi
+        if x[1] < cxe:
+            ATan1 += math.pi
+        elif ATan1 < 0:
+            ATan1 += 2*math.pi
+    elif ATan0 < 0:
+        ATan0 += 2*math.pi
+        if x[1] < cxe:
+            ATan1 += math.pi
+        else:
+            ATan1 += 2*math.pi
+    else:
+        if x[1] < cxe:
+            ATan1 += math.pi
+        elif ATan1 < 0:
+            ATan1 += 2*math.sin(phi)
+            ATan0 += 2*math.sin(phi)
+
+    q = 1
+    if ATan1 < ATan0:
+        q = -1
+    
+    u *= q
+    D *= q
+    if q < 0:
+        if phi > math.pi:
+            phi -= math.pi
+        else:
+            phi += math.pi
+
+    return (u, D, phi)
+
+
 class Tracking:
-#    def __int__(self):
+    def __init__(self,nhit_max):
+        self.df = pd.DataFrame()
+        self.nhit_max = nhit_max
 
     def get_track_id(self):
-        return self.track_id
+        return self.df.track_id.values
 
     def get_hit_id(self):
-        return self.hit_id
+        hit_id = np.zeros((ndata,nhit_max))
+        for ihit in range(self.nhit_max):
+            hit_id[ihit] = self['hit_id_' + str(ihit)].transpose()
+        return hit_id
 
     def get_state(self):
-        return self.state
+        return np.array([self.df.u.values,self.df.D.values,self.df.phi.values]).transpose()
 
     def get_state_cov(self):
-        return self.state_cov
+        cov_u = np.array([self.df.cov_uu.values,self.df.cov_Du.values,self.df.cov_pu.values])
+        cov_D = np.array([self.df.cov_uD.values,self.df.cov_DD.values,self.df.cov_pD.values])
+        cov_p = np.array([self.df.cov_up.values,self.df.cov_Dp.values,self.df.cov_pp.values])
+        return np.array([cov_u,cov_D,cov_p]).transpose()
   
     def get_sum_chi2(self):
-        return self.sum_chi2
+        return self.df.sum_chi2.values
 
     def set_track_id(self,track_id):
-        self.track_id = track_id
+        self.df['track_id'] = track_id
    
     def set_hit_id(self,hit_id):
-        self.hit_id = hit_id
+        for ihit in range(self.nhit_max):
+            self.df['hit_id_' + str(ihit)] = hit_id[:,ihit]
    
     def set_state(self,state):
-        self.state = state
+        self.df['u'] = state[:,0]
+        self.df['D'] = state[:,1]
+        self.df['phi'] = state[:,2]
 
     def set_state_cov(self,state_cov):
-        self.state_cov = state_cov
+        self.df['cov_uu'] = state_cov[:,0,0]
+        self.df['cov_uD'] = state_cov[:,0,1]
+        self.df['cov_up'] = state_cov[:,0,2]
+        self.df['cov_Du'] = state_cov[:,1,0]
+        self.df['cov_DD'] = state_cov[:,1,1]
+        self.df['cov_Dp'] = state_cov[:,1,2]
+        self.df['cov_pu'] = state_cov[:,2,0]
+        self.df['cov_pD'] = state_cov[:,2,1]
+        self.df['cov_pp'] = state_cov[:,2,2]
 
     def set_sum_chi2(self,sum_chi2):
-        self.sum_chi2 = sum_chi2
+        self.df['sum_chi2'] = sum_chi2
 
     def add_track_id(self,track_id):
-        np.append(self.track_id,track_id)
+        np.append(self.df.track_id.values,track_id)
 
     def add_hit_id(self,hit_id):
-        np.append(self.hit_id,hit_id)
+        np.append(self.df.hit_id.values,hit_id)
 
     def add_state(self,state):
-        np.append(self.state,state)
+        np.append(self.df.state.values,state)
 
     def add_state_cov(self,state_cov):
-        np.append(self.state_cov,state_cov)
+        np.append(self.df.state_cov.values,state_cov)
 
 
     def add_sum_chi2(self,sum_chi2):
-        np.append(self.sum_chi2,sum_chi2)
+        np.append(self.df.sum_chi2.values,sum_chi2)
 
     def cut_by_chi2(self,limit):
-        cut = self.sum_chi2 > limit
-        track_id = np.zeros(self.track_id.shape[0])
-        hit_id = np.zeros((self.hit_id.shape[0],self.hit_id.shape[1]))
-        state = np.zeros((self.state.shape[0],self.state.shape[1]))
-        state_cov = np.zeros((self.state_cov.shape[0],self.state_cov.shape[1],self.state_cov.shape[2]))
-        sum_chi2 = np.zeros(self.sum_chi2.shape[0])
+        cut = self.df.sum_chi2 > limit
+        track_id = np.zeros(len(self.df.track_id))
+        hit_id = np.zeros((self.get_hit_id().shape[0],self.get_hit_id().shape[1]))
+        state = np.zeros((self.get_state().shape[0],self.get_state().shape[1]))
+        state_cov = np.zeros((self.get_state_cov().shape[0],self.get_state_cov().shape[1],self.get_state_cov().shape[2]))
+        sum_chi2 = np.zeros(len(self.df.sum_chi2))
         n = 0
         for i in range(len(cut)):
             if cut[i]:
                 continue
-            track_id[n] = self.track_id[i]
-            hit_id[n] = self.hit_id[i]
-            state[n] = self.state[i]
-            state_cov[n] = self.state_cov[i]
+            track_id[n] = self.df.track_id[i]
+            hit_id[n] = self.get_hit_id()[i]
+            state[n] = self.get_state()[i]
+            state_cov[n] = self.get_state_cov()[i]
             sum_chi2[n] = self.sum_chi2[i]
             n += 1
-        self.track_id = track_id[0:n]
-        self.hit_id = hit_id[0:n,:]
-        self.state = state[0:n,:]
-        self.state_cov = state_cov[0:n,:,:]
-        self.sum_chi2 = sum_chi2[0:n]
+        self.df.track_id.values = track_id[0:n]
+        self.set_hit_id(hit_id[0:n,:])
+        self.set_state(state[0:n,:])
+        self.set_state_cov(state_cov[0:n,:,:])
+        self.df.sum_chi2.values = sum_chi2[0:n]
     
-        print("removed ", len(cut) - n , "track candidates")
+        print('removed ', len(cut) - n , 'track candidates')
 
