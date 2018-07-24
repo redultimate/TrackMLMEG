0b3b9a1a5ee1fb7b6a322624a550203676d11da1
Mitsutaka Nakao <nakao@icepp.s.u-tokyo.ac.jp>
2018-07-24 12:06:32 +0200

    prepared look-up table for 13_6 to 13_8

--- lib/seeding_utility.py
+++ lib/seeding_utility.py
@@ -4,9 +4,11 @@ import pickle as pkl
 width = 15
 mid_lookup_17_4_to_17_2 = []
 mid_lookup_17_2_to_13_8 = []
+mid_lookup_13_6_to_13_8 = []
 modules_17_4 = 3192
 modules_17_2 = 2520
 modules_13_8 = 2142
+modules_13_6 = 1638
 
 def func_data(x, y, slope, offset):
     return abs(slope * x + offset - y)
@@ -53,6 +55,24 @@ def make_module_tables(vid, path_to_output):
         f = open('%s/mid_lookup_17_2_to_13_8.pkl' % path_to_output, 'wb')
         pkl.dump(mid_lookup_17_2_to_13_8, f)
 
+    elif vid == 13:
+        for i in range(modules_13_6):
+            x = i+1
+            temp = []
+            for j in range(modules_13_8):
+                y = j+1
+                if (   (func_data(x, y, 1.31, -0.34) <= width) or 
+                        ((func_data(x, y, 1.31, 100.3) <= width) and (x > 400) and (x < 1400))or
+                        ((func_data(x, y, 1.31, 199.4) <= width) and (x > 700) and (x < 1300)) or
+                        ((func_data(x, y, 1.31, -101.3) <= width)  and (x < 1200))or
+                        ((func_data(x, y, 1.31, -204.6 ) <= width) and (x > 300) and (x < 800))
+                   ):
+                    temp.append(y)
+            mid_lookup_13_6_to_13_8.append(temp)
+
+        # return mid_lookup_17_4_to_17_2, mid_lookup_17_2_to_13_8
+        f = open('%s/mid_lookup_13_6_to_13_8.pkl' % path_to_output, 'wb')
+        pkl.dump(mid_lookup_13_6_to_13_8, f)
     else:
-        print('warning!! choose volume id from (17). Othre modules will be implemented.')
+        print('warning!! choose volume id from (17, 13). Othre modules will be implemented.')
         # return 0, 0
